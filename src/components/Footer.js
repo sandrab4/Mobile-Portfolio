843849b237a8e2ac5610702f16ba7ba5ca0e0128
Sandra Barron <sandra@Sandras-MBP.lan>
2023-03-23 11:59:46 -0500

    updated colorsand text

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from "react-bootstrap";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,42 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>skills</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
