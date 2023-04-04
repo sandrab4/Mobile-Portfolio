f8e8feea177e971876fab995730ea4e7d29e1484
Sandra Barron <sandra@Sandras-MBP.lan>
2023-04-04 12:05:42 -0500

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,37 +1,9 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import picbg from "../assets/img/picbg.jpg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "fix" ];
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-  }
-
   return (
     <section className="banner" id="home">
       <Container>
@@ -40,9 +12,8 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h1>{`Sandra Barron `} <br></br>
-                 <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Welcome To My Portfolio Website!</p>
+                <h1>{`Sandra Barron `} <br></br></h1>
+                  <p>Web Developer</p>
               </div>}
             </TrackVisibility>
           </Col>
