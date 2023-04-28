9b4b9a0a0342fb4b4d302b4618f54ee5ada81a14
Sandra Barron <sandra@Sandras-MBP.lan>
2023-04-27 22:46:51 -0500

    new

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1 style={{fontSize: '36px', color: 'black'}}>Sandra Barron</h1>
+      <p style={{fontSize: '24px', color: 'black'}} >
+        Wev Developer
+      </p>
+    </div>
+  );
+};
+
+export default Home;
