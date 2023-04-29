b9e9230c0e2a282728f91501fc991c863e26ecde
Sandra Barron <sandra@Sandras-MBP.lan>
2023-04-28 21:42:56 -0500

    components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h2>About</h2>
-      <p style={{color: 'black'}}>
+    <div style={{textAlign: 'center', color: 'black'}}>
+      <h2 style={{fontSize: '2rem'}}>About</h2>
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod ante libero,
         in faucibus nisi bibendum non. Donec euismod leo vel elit dapibus congue.
         Praesent sit amet enim et lorem elementum bibendum eget in ex. Vivamus suscipit mi id nulla vestibulum,
