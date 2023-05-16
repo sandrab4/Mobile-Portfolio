848755c060ad7a96e118a648ebc5af6afb98eebd
Sandra Barron <sandra@Sandras-MBP.lan>
2023-05-15 22:03:44 -0500

    projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-import SlideUp from "./SlideUp"
-import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+"use client"
+import React from "react";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 
 const projects = [
   {
@@ -15,20 +15,19 @@ const projects = [
   },
   {
     name: "Task App",
-    description: "This is a to do list app that I built using React Native.",
-    video: "/coming.png",
+    description: "This is a to-do list app that I built using React Native.",
+    video: "/taskAppDemo.mp4",
     github: "",
     link: "",
   },
   {
     name: "Cat Island Game",
-    description:
-      "Comin Soon",
-    video: "/coming.png",
+    description: "Coming Soon",
+    video: "/",
     github: "",
     link: "",
   },
-]
+];
 
 const Projects = () => {
   return (
@@ -42,19 +41,24 @@ const Projects = () => {
           return (
             <div key={idx}>
               <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
+                <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className="md:w-1/2">
                     <Link href={project.link} target="_blank">
-                      <video
-                        src={project.video}
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                      />
+                      <div className="video-container rounded-xl">
+                        <video
+                          src={project.video}
+                          autoPlay
+                          loop
+                          muted
+                          className="video-element"
+                        />
+                      </div>
                     </Link>
                   </div>
                   <div className="mt-8 md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-6 text-black">{project.name}</h1>
+                    <h1 className="text-4xl font-bold mb-6 text-black">
+                      {project.name}
+                    </h1>
                     <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
                       {project.description}
                     </p>
@@ -76,12 +80,27 @@ const Projects = () => {
                 </div>
               </SlideUp>
             </div>
-          )
+          );
         })}
-        
       </div>
+
+      <style jsx>{`
+        .video-container {
+          position: relative;
+          padding-bottom: 80%;
+          overflow: hidden;
+        }
+
+        .video-element {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+      `}</style>
     </section>
-  )
-}
+  );
+};
 
 export default Projects;
