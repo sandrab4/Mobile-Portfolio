ef41cbab7b982753d7b5bdc271613a1fb1190283
Sandra Barron <sandra@Sandras-MBP.lan>
2023-05-18 22:42:28 -0500

    icons

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,16 +1,20 @@
-import React from "react"
+import React from "react";
+import reactIcon from "./reactIcon.png";
+import nextjsIcon from "./nextjsIcon.png";
 
-const Skills = () => {
+const Skills: React.FC = () => {
   return (
     <section id="skills">
       <div className="my-12 pb-12 md:pt-16 md:pb-48">
-        <h1 className="text-center font-bold text-4xl text-black">
-          Skills
-        </h1>
-        <h1 className="text-black">Icons</h1>
-          </div>
+        <h1 className="text-center font-bold text-4xl text-black">Skills</h1>
+        <img src="reactIcon.png" alt="React Icon" style={{ width: "100px", height: "90px" }}  />
+        <p className="text-black">
+          React icon, Next.js icon, Node.js icon, TypeScript icon, JavaScript icon
+        </p>
+        <p>SMALLICONS git, expo, figma, tailwind css, html, css blender, xcode</p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Skills;
